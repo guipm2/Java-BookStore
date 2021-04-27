cefe5ae175e862810c9372cefee30d8bd0a7950d
guipm2 <guipm2@gmail.com>
2021-04-27 14:25:14 -0300

    ready

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -62,6 +62,14 @@ public class ControllerServlet extends HttpServlet {
  				case "/delete":
  					deleteBook(request, response);
 					break
+				case "/edit":
+					showEditForm(request, response);
+					break;
+				case "/update":
+					updateBook(request, response);
+					break;
+		updateBook(request, response);
+		break;
         default:
 				   listBooks(request, response);
            break;
@@ -108,12 +116,36 @@ public class ControllerServlet extends HttpServlet {
 		response.sendRedirect("list");
 	}
 
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Book book = bookDAO.getBook(id);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		request.setAttribute("book", book);
+		dispatcher.forward(request, response);
+	}
+
+
 	private void deleteBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
 		bookDAO.deleteBook(id);
 
 		response.sendRedirect("list");
 	}
+
+	private void updateBook(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+			int id = Integer.parseInt( request.getParameter("id") );
+			String title = request.getParameter("booktitle");
+			String author = request.getParameter("bookauthor");
+			String price = request.getParameter("bookprice");
+
+			Book newBook = new Book(id, title, author, Float.parseFloat(price) );
+
+			bookDAO.updateBook(newBook);
+			response.sendRedirect("list");
+		}
+
 
 
 	/**
